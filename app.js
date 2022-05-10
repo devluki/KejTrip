c945375592d63ca5167c6c995bf438ff4f6d5355
devluki <lukasz.zabiegli@gmail.com>
2022-05-10 22:40:03 +0200

    test

--- app.js
+++ app.js
@@ -69,14 +69,7 @@ io.on("connection", (socket) => {
 
 })
 
-// Test
-var HOST = location.origin.replace(/^http/, 'ws')
-var ws = new WebSocket(HOST);
-var el = document.getElementById('server-time');
-ws.onmessage = function (event) {
-    el.innerHTML = 'Server time: ' + event.data;
-};
-// Test
+
 
 // Routes
 
